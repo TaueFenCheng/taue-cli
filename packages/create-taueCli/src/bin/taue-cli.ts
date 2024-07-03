8bec33f95a7e3d48eb82693ee7b6002abab652d6
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-07-03 14:48:48 +0800

    feat: add

--- packages/create-taueCli/src/bin/taue-cli.ts
+++ packages/create-taueCli/src/bin/taue-cli.ts
@@ -1,5 +1,27 @@
 #!/usr/bin/env node
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { Command } from "commander"
+import minimist from 'minimist'
+
+const argv = minimist<{
+    template?: string
+    help?: boolean
+  }>(process.argv.slice(2), {
+    default: { help: false },
+    alias: { h: 'help', t: 'template' },
+    string: ['_'],
+  })
+const cwd = process.cwd()
+const defaultTargetDir = 'taue-project'
+function formatTargetDir(targetDir: string | undefined) {
+    return targetDir?.trim().replace(/\/+$/g, '')
+}
+const argTargetDir = formatTargetDir(argv._[0])
+let targetDir = argTargetDir || defaultTargetDir
+const root = path.join(cwd, targetDir)
+
 
 const program = new Command()
 console.log(process.argv.slice(2));
